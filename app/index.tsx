5b7a719aae5605e36236478d7f8839bb7ee62bfd
abhilash2616 <abhilashbera2003@gmail.com>
2026-03-23 22:34:54 +0530

    feat: Initialize Expo project with Expo Router, NativeWind, and Tailwind CSS configuration.

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-red-700">
+      <Text className="text-white">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
